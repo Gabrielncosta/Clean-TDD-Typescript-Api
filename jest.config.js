d40e25654ac06d3e33594e670f48886e3a6e0412
Gabriel <gabrielncosta208@gmail.com>
2021-02-15 15:15:42 -0300

    chore: add jest config for mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,15 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.*\\.ts$': 'ts-jest'
   }
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
-
-  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
-  // unmockedModulePathPatterns: undefined,
-
-  // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
-
-  // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-  // watchPathIgnorePatterns: [],
-
-  // Whether to use watchman for file crawling
-  // watchman: true,
 }
