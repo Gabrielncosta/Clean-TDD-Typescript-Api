3684ce845fa32bb0c17bc4db196335d1bd82db39
Gabriel <gabrielncosta208@gmail.com>
2021-02-18 21:09:54 -0300

    feat: ensure mongodb reconnect if client is down

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup'
 import { adaptExpressRoute } from '../adapters/express-route-adapter'
+
 export default (router: Router): void => {
   router.post('/signup', adaptExpressRoute(makeSignUpController()))
 }
